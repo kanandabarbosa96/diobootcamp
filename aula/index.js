0b8a466f0a86c4de9454d8d1eb7ea7a7a97db239
kanandabarbosa <kanandabarbosa96@gmail.com>
2024-10-08 20:52:27 -0300

    first commit

---
+++ aula/index.js
@@ -0,0 +1,30 @@
+let nome = "Mulher Maravilha"
+let xp = 2800
+let nivel = ""
+
+if (xp < 1000){
+    nivel = "Ferro"
+}
+else if(xp >= 1001 && xp < 2001){
+    nivel = "Bronze"
+}
+else if (xp >= 2001 && xp <5001){
+    nivel = "prata"
+}
+else if(xp >= 5001 && xp < 7001){
+    nivel = "Ouro"
+}
+else if(xp >= 7001 && xp < 8001){
+    nivel = "Platina"
+}
+else if(xp >= 8001 && xp < 9001){
+    nivel = "Ascendente"
+}
+else if(xp >= 9001 && xp <10001){
+    nivel = "Imortal"
+}
+else{
+    nivel = "Radiante"
+}
+
+console.log("o Herói de nome",nome,"está no nivel de",nivel)
